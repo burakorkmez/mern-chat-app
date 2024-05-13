da3ffc1c58fe3eef04504cce3438029dc24edc23
Aniket Singh <aniketsingh2704@gmail.com>
2024-05-13 16:58:01 +0530

    url brackets

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,20 +1,17 @@
 import mongoose from "mongoose";
-const dbUrl =
-  process.env.DB_URL ||
+const url =
   "mongodb+srv://aniketsingh2704:lFMzlwdk4fFIE4IX@chat-app-cluster.h8genz4.mongodb.net/chat-app-db";
-
-
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.dbUrl, {
+  try {
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // Other options if needed
     });
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 
 export default connectToMongoDB;
