ed0e6807229b4039c6073f614885cc87e3d57fc6
Kalprajsinh <kalpraj51@gmail.com>
2024-06-19 16:28:48 +0530

    login id not showing #21

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,7 +4,7 @@ export const getUsersForSidebar = async (req, res) => {
 	try {
 		const loggedInUserId = req.user._id;
 
-		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+		const filteredUsers = await User.find().select("-password");
 
 		res.status(200).json(filteredUsers);
 	} catch (error) {
